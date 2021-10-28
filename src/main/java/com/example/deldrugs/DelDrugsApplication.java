afc517480f592bf302744fa8e4e30bf6ef84eaf9
Evgeniy2312 <evgen.matvee1986@gmail.com>
2021-10-28 12:11:38 +0300

    init

---
+++ src/main/java/com/example/deldrugs/DelDrugsApplication.java
@@ -0,0 +1,13 @@
+package com.example.deldrugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelDrugsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelDrugsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deldrugs/DelDrugsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deldrugs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelDrugsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
